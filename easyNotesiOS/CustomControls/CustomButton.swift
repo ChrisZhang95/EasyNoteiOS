4260eb4d930f34a3bfda298a8e2e9f3c8891f541
ChrisZhang95 <chris.zhang95@hotmail.com>
2018-06-11 22:10:38 -0400

    Initial commit

---
+++ easyNotesiOS/CustomControls/CustomButton.swift
@@ -0,0 +1,27 @@
+//
+//  CustomButton.swift
+//  easyNotesiOS
+//
+//  Created by Chris Zhang on 2018-06-11.
+//  Copyright © 2018 Chris Zhang. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CustumButton: UIButton {
+    
+    @IBInspectable var customButton:Bool = false {
+        didSet {
+            if customButton {
+                layer.cornerRadius = frame.height / 2
+            }
+        }
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        if customButton {
+            layer.cornerRadius = frame.height / 2
+        }
+    }
+}
