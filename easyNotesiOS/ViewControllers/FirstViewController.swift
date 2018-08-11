17b07cf3d23c04adbe77c84b06e75104c53d88c9
ChrisZhang95 <chris.zhang95@hotmail.com>
2018-08-10 22:10:38 -0400

    Http request implemented

--- easyNotesiOS/ViewControllers/FirstViewController.swift
+++ easyNotesiOS/ViewControllers/FirstViewController.swift
@@ -7,9 +7,26 @@
 //
 
 import UIKit
-
+import Alamofire
 class FirstViewController: UIViewController {
 
+    //Handles button click, function sends a http request to python machine learning backend server and retrieve a response
+    @IBAction func httpRequestButtonClick(_ sender: Any) {
+        //This is the ip address of the computer that flask server is running on. Inside python code, set app.run(host= '<ip_adress>') to the same address
+        Alamofire.request("http://192.168.0.19:5000/").responseJSON { response in
+            print("Request: \(String(describing: response.request))")   // original url request
+            print("Response: \(String(describing: response.response))") // http url response
+            print("Result: \(response.result)")                         // response serialization result
+            
+            if let json = response.result.value {
+                print("JSON: \(json)") // serialized json response
+            }
+            
+            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                print("Data: \(utf8Text)") // original server data as UTF8 string
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
