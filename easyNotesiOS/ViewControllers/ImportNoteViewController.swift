a180230875b3358187c44210b0a96bd4eae43401
ChrisZhang95 <chris.zhang95@hotmail.com>
2018-09-14 14:50:00 -0400

    -Moved share button to the top of the navigation bar -Photo editing implemented

--- easyNotesiOS/ViewControllers/ImportNoteViewController.swift
+++ easyNotesiOS/ViewControllers/ImportNoteViewController.swift
@@ -10,17 +10,29 @@ import UIKit
 
 class ImportNoteViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    //Drawing
+    var lastPoint = CGPoint.zero
+    var red: CGFloat = 0.0
+    var green: CGFloat = 0.0
+    var blue: CGFloat = 0.0
+    var brushWidth: CGFloat = 10.0
+    var opacity: CGFloat = 1.0
+    var swiped = false
+    
     
     @IBOutlet weak var myImg: UIImageView!
+    //@IBOutlet weak var tempImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Share", style: .plain, target: self, action: #selector(shareButtonClick))
+        
         // Create UIImagePickerController on load up
         let image = UIImagePickerController()
         image.delegate = self
         image.sourceType = UIImagePickerControllerSourceType.photoLibrary
-        image.allowsEditing = true
+        //image.allowsEditing = true
         
         //Present UIImagePickerController on UI
         self.present(image, animated: true){
@@ -45,11 +57,74 @@ class ImportNoteViewController: UIViewController, UIImagePickerControllerDelegat
         self.dismiss(animated: true, completion: nil)
     }
     
-    @IBAction func shareButtonClick(_ sender: UIButton) {
+    @objc func shareButtonClick() {
         //Share image
         let activityController = UIActivityViewController(activityItems: [myImg.image!], applicationActivities: nil)
         present(activityController, animated: true, completion: nil)
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        swiped = false
+        if let touch = touches.first {
+            lastPoint = touch.location(in: self.view)
+        }
+    }
+    
+    //Function that draws a line
+    func drawLineFrom(fromPoint: CGPoint, toPoint: CGPoint) {
+
+        // 1
+        UIGraphicsBeginImageContext(view.frame.size)
+        let context = UIGraphicsGetCurrentContext()
+        myImg.image?.draw(in: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height))
+
+        // 2
+        context?.move(to: CGPoint(x: fromPoint.x, y: fromPoint.y))
+        context?.addLine(to: CGPoint(x: toPoint.x, y: toPoint.y))
+
+        // 3
+        context!.setLineCap(.round)
+        context!.setLineWidth(brushWidth)
+        context!.setBlendMode(.normal)
+        context?.setStrokeColor(red:1, green: 1, blue: 1, alpha:1)
+
+        // 4
+        context!.strokePath()
+
+        // 5
+        myImg.image = UIGraphicsGetImageFromCurrentImageContext()
+        myImg.alpha = opacity
+        UIGraphicsEndImageContext()
+
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // 6
+        swiped = true
+        if let touch = touches.first {
+            let currentPoint = touch.location(in: view)
+            drawLineFrom(fromPoint: lastPoint, toPoint: currentPoint)
+            
+            // 7
+            lastPoint = currentPoint
+        }
+    }
+    
+//    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        if !swiped {
+//            // draw a single point
+//            drawLineFrom(fromPoint: lastPoint, toPoint: lastPoint)
+//        }
+//
+//        // Merge tempImageView into mainImageView
+//        UIGraphicsBeginImageContext(myImg.frame.size)
+//        myImg.image?.draw(in: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height), blendMode: .normal, alpha: 1.0)
+//        tempImageView.image?.draw(in: CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height), blendMode: .normal, alpha: opacity)
+//        myImg.image = UIGraphicsGetImageFromCurrentImageContext()
+//        UIGraphicsEndImageContext()
+//
+//        tempImageView.image = nil
+//    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
